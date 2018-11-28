731392d5ea08821eb592afe8683a52488de40c4b
WilliamNicholsx <45142810+WilliamNicholsx@users.noreply.github.com>
2018-11-29 10:32:30 +1100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -48,6 +48,7 @@ function getMovies(search) {
     }
     if ($('#movies').length) {
 	$('.container > h1').hide();
+    }
 
     
     axios.get("https://api.themoviedb.org/3/search/movie?api_key=bb3435e6fa75215145fc17204790ac6c&language=en-US&query=" + search + "&page=1&include_adult=false")
